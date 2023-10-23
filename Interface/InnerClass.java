cc54712292482094d3e02f00448b596cbbf63794
Mahfuzar Rahman <113121842+Mahfuzar148@users.noreply.github.com>
2023-10-23 21:54:56 +0600

    Create InnerClass.java

---
+++ Interface/InnerClass.java
@@ -0,0 +1,26 @@
+public class OuterClass {
+    void showOuter(){
+        System.out.println("This is outer class");
+    }
+    public class InnerClass{
+        void showInner(){
+            System.out.println("This is inner class");
+        }
+        public class InnerInnerClass{
+            void showInnerInnerClass(){
+                System.out.println("This is innerInner class");
+            }
+        }
+    }
+}
+class OuterClassTester1 {
+    public static void main(String[] args) {
+        OuterClass outerClass = new OuterClass();
+        outerClass.showOuter();
+        OuterClass.InnerClass innerClass=  outerClass.new InnerClass();
+        innerClass.showInner();
+        OuterClass.InnerClass.InnerInnerClass innerInnerClass = innerClass.new InnerInnerClass();
+        innerInnerClass.showInnerInnerClass();
+    }
+
+}
